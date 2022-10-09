25084aea0aaccfdc5187c72fc8097ca666a40423
Raxipaxi <razielferundir@gmail.com>
2022-10-08 22:17:27 -0300

    Connect scene
    Main a Co-Op y Connect andando

--- Assets/_Main/Scripts/Menuse/Network/ConnectToServer.cs
+++ Assets/_Main/Scripts/Menuse/Network/ConnectToServer.cs
@@ -1,12 +1,23 @@
+using System;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     public TMP_InputField userInput;
     public TMP_Text buttonText;
+    public Button connectButton;
+    public Sprite _onPointer;
+    private Sprite _defaultSprite;
 
+    private void Awake()
+    {
+        _defaultSprite = connectButton.image.sprite;
+    }
 
     public void OnClickConnect()
     {
@@ -23,5 +34,13 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
         SceneManager.LoadScene("Lobby");
     }
 
+    public void OnMouseEnter()
+    {
+        connectButton.image.sprite = _onPointer;
+    }
 
+    public void OnMouseExit()
+    {
+        connectButton.image.sprite = _defaultSprite;
+    }
 }
