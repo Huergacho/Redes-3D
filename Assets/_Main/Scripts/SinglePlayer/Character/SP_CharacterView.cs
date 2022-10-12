add176e3a1189f97ed7c86d927116ecc69d5620b
Facundo Huergo <facuhuergo@hotmail.com>
2022-10-11 21:26:07 -0300

    Atado con alambre pero funca el point counter en sp

--- Assets/_Main/Scripts/SinglePlayer/Character/SP_CharacterView.cs
+++ Assets/_Main/Scripts/SinglePlayer/Character/SP_CharacterView.cs
@@ -7,11 +7,14 @@ using UnityEngine.Serialization;
 public class SP_CharacterView : MonoBehaviourPun
 {
     [SerializeField]protected Identificator identificatorPrefab;
+
+    [SerializeField] protected SP_PointCounter spPointCounter;
     // Start is called before the first frame update
     void Start()
     {
         identificatorPrefab.SetTarget(this.transform);
         identificatorPrefab.SetColor(1);
+        spPointCounter.SetColor(identificatorPrefab.GetColor());
     }
 
     // Update is called once per frame
